29e25d9244c433d9f5bfab8e32808c21c631ce45
Roderic Bos <github@rooc.nl>
2023-06-13 21:57:42 +0200

    color tweak

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -2,29 +2,30 @@ local colors = {
   blue   = '#80a0ff',
   cyan   = '#79dac8',
   black  = '#080808',
+  white2  = '#868686',
   white  = '#c6c6c6',
   red    = '#ff5189',
   violet = '#d183e8',
   grey   = '#303030',
 }
 
-local bubbles_theme = {
+local rc_theme = {
   normal = {
     a = { fg = colors.black, bg = colors.violet },
     b = { fg = colors.white, bg = colors.grey },
-    c = { fg = colors.violet, bg = colors.black },
+	c = { fg = colors.black, bg = colors.white2 },
   },
 
-  insert = { a = { fg = colors.black, bg = colors.blue } },
+  insert = { a = { fg = colors.black, bg = colors.blue }, c = { fg = colors.black, bg = colors.blue } },
   visual = { a = { fg = colors.black, bg = colors.cyan } },
   command = { a = { fg = colors.black, bg = colors.red } },
   replace = { a = { fg = colors.black, bg = colors.red } },
 
   inactive = {
-    a = { fg = colors.black, bg = colors.black },
-    b = { fg = colors.black, bg = colors.black },
-    c = { fg = colors.white, bg = colors.black },
+    a = { fg = colors.black, bg = colors.grey },
+    b = { fg = colors.black, bg = colors.grey },
+    c = { fg = colors.black, bg = colors.grey },
   },
 }
 
-require('lualine').setup({ options = { theme = bubbles_theme } })
+require('lualine').setup({ options = { theme = rc_theme } })
