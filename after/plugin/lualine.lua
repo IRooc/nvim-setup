ae11c48ecf2842cc3af15dee44527efe27afa1fe
Roderic Bos <github@rooc.nl>
2023-06-14 12:00:20 +0200

    update lualine status more often

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,31 +1,38 @@
 local colors = {
-  blue   = '#80a0ff',
-  cyan   = '#79dac8',
-  black  = '#080808',
-  white2  = '#868686',
-  white  = '#c6c6c6',
-  red    = '#ff5189',
-  violet = '#d183e8',
-  grey   = '#303030',
+	blue   = '#80a0ff',
+	cyan   = '#79dac8',
+	black  = '#080808',
+	white2 = '#868686',
+	white  = '#c6c6c6',
+	red    = '#ff5189',
+	violet = '#d183e8',
+	grey   = '#303030',
 }
 
 local rc_theme = {
-  normal = {
-    a = { fg = colors.black, bg = colors.violet },
-    b = { fg = colors.white, bg = colors.grey },
-	c = { fg = colors.black, bg = colors.white2 },
-  },
+	normal = {
+		a = { fg = colors.black, bg = colors.violet },
+		b = { fg = colors.white, bg = colors.grey },
+		c = { fg = colors.black, bg = colors.white2 },
+	},
 
-  insert = { a = { fg = colors.black, bg = colors.blue }, c = { fg = colors.black, bg = colors.blue } },
-  visual = { a = { fg = colors.black, bg = colors.cyan } },
-  command = { a = { fg = colors.black, bg = colors.red } },
-  replace = { a = { fg = colors.black, bg = colors.red } },
+	insert = { a = { fg = colors.black, bg = colors.blue }, c = { fg = colors.black, bg = colors.blue } },
+	visual = { a = { fg = colors.black, bg = colors.cyan } },
+	command = { a = { fg = colors.black, bg = colors.red } },
+	replace = { a = { fg = colors.black, bg = colors.red } },
 
-  inactive = {
-    a = { fg = colors.black, bg = colors.grey },
-    b = { fg = colors.black, bg = colors.grey },
-    c = { fg = colors.black, bg = colors.grey },
-  },
+	inactive = {
+		a = { fg = colors.black, bg = colors.grey },
+		b = { fg = colors.black, bg = colors.grey },
+		c = { fg = colors.black, bg = colors.grey },
+	},
 }
 
-require('lualine').setup({ options = { theme = rc_theme } })
+require('lualine').setup({
+	options = {
+		refresh = {
+			statusline = 200,
+		},
+		theme = rc_theme
+	}
+})
